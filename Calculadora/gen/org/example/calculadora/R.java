bce6cd75d2c1d8a7bdab4fc3dac415ee96b36a16
curso3 <curso3@curso3.(none)>
2013-04-10 10:37:07 +0200

    cambio de estilo botones

--- Calculadora/gen/org/example/calculadora/R.java
+++ Calculadora/gen/org/example/calculadora/R.java
@@ -10,6 +10,20 @@ package org.example.calculadora;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int article_title=0x7f04000a;
+        public static final int articlecolor=0x7f040009;
+        public static final int black=0x7f040004;
+        public static final int blue=0x7f040006;
+        public static final int blueback=0x7f040008;
+        public static final int cachecolor=0x7f04000b;
+        public static final int date_color=0x7f040003;
+        public static final int gold=0x7f040007;
+        public static final int gray=0x7f040005;
+        public static final int orange=0x7f040000;
+        public static final int transparent=0x7f040002;
+        public static final int white=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,58 +31,61 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int green_button=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080016;
-        public static final int btn0=0x7f080012;
-        public static final int btn1=0x7f08000e;
-        public static final int btn2=0x7f08000f;
-        public static final int btn3=0x7f080010;
-        public static final int btn4=0x7f08000a;
-        public static final int btn5=0x7f08000b;
-        public static final int btn6=0x7f08000c;
-        public static final int btn7=0x7f080006;
-        public static final int btn8=0x7f080007;
-        public static final int btn9=0x7f080008;
-        public static final int btnBorrar=0x7f080002;
-        public static final int btnBorrarCaracter=0x7f080005;
-        public static final int btnDividir=0x7f080003;
-        public static final int btnIgual=0x7f080015;
-        public static final int btnMas=0x7f08000d;
-        public static final int btnMenos=0x7f080009;
-        public static final int btnMultiplicar=0x7f080004;
-        public static final int btnParentesis=0x7f080011;
-        public static final int btnPunto=0x7f080013;
-        public static final int btnSigno=0x7f080014;
-        public static final int gridBotones=0x7f080001;
-        public static final int textoResultado=0x7f080000;
+        public static final int action_settings=0x7f090016;
+        public static final int btn0=0x7f090012;
+        public static final int btn1=0x7f09000e;
+        public static final int btn2=0x7f09000f;
+        public static final int btn3=0x7f090010;
+        public static final int btn4=0x7f09000a;
+        public static final int btn5=0x7f09000b;
+        public static final int btn6=0x7f09000c;
+        public static final int btn7=0x7f090006;
+        public static final int btn8=0x7f090007;
+        public static final int btn9=0x7f090008;
+        public static final int btnBorrar=0x7f090002;
+        public static final int btnBorrarCaracter=0x7f090005;
+        public static final int btnDividir=0x7f090003;
+        public static final int btnIgual=0x7f090015;
+        public static final int btnMas=0x7f09000d;
+        public static final int btnMenos=0x7f090009;
+        public static final int btnMultiplicar=0x7f090004;
+        public static final int btnParentesis=0x7f090011;
+        public static final int btnPunto=0x7f090013;
+        public static final int btnSigno=0x7f090014;
+        public static final int gridBotones=0x7f090000;
+        public static final int textoResultado=0x7f090001;
     }
     public static final class layout {
         public static final int calculadora=0x7f030000;
     }
     public static final class menu {
-        public static final int calculadora=0x7f070000;
+        public static final int calculadora=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int borrado=0x7f050003;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int borrado=0x7f060003;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+
     
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
         
 
         Base application theme for API 11+. This theme completely replaces
@@ -82,10 +99,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+        public static final int ButtonText=0x7f070002;
     }
 }
